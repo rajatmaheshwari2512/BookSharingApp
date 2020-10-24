b5aea8fe01a70398194e66236f3532114913c13b
Rajat <mrajat67@yahoo.com>
2020-10-24 20:14:19 +0530

    Fixed Paths

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const mongoose = require("mongoose");
 const Grid = require("gridfs-stream");
 const GridFsStorage = require("multer-gridfs-storage");
 const multer = require("multer");
-const Fuse = require('fuse.js');
-const Books = require('./model/Books');
+const Fuse = require("fuse.js");
+const Books = require("./models/Books");
 
 const userRouter = require("./routes/userRouter"); //Hiten put your files in userRouter
 const uploadRouter = require("./routes/uploadRouter"); //Ankit put your files in uplaodRouter
@@ -57,37 +57,30 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-
-//BOOKS DATABASE LIST    
+//BOOKS DATABASE LIST
 const books = [];
 Books.find({}, (err, collections) => {
-    collections.forEach(data => { books.push(data) });
+  collections.forEach((data) => {
+    books.push(data);
+  });
 });
-
-
 
 //Routers
 //app.use("/users", userRouter);
 //app.use("/upload", uploadRouter);
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/stylesheets/index.html");
+});
 
-//Takes the query from the input, search from the database and give the results 
-app.post('/', (req, res) => {
-    let bookQuery = req.body.bookQuery;
-    console.log(bookQuery);
-    const fuse = new Fuse(books, { keys: ['name', 'author'] });
-    const results = fuse.search(bookQuery);
-    console.log(results);
-    res.send('book');
-})
-
-
-
-
-
-
+//Takes the query from the input, search from the database and give the results
+app.post("/", (req, res) => {
+  let bookQuery = req.body.bookQuery;
+  console.log(bookQuery);
+  const fuse = new Fuse(books, { keys: ["name", "author"] });
+  const results = fuse.search(bookQuery);
+  console.log(results);
+  res.send("book");
+});
 
 app.use(function (req, res, next) {
   next(createError(404));
